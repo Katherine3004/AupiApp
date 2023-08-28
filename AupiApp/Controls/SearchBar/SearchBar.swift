e979ff17baf095a1fbb83cc4008d1420991d1bb4
Katherine Chambers <katherine@mad9000.com>
2023-08-28 19:11:03 +0200

    interface state

--- AupiApp/Controls/SearchBar/SearchBar.swift
+++ AupiApp/Controls/SearchBar/SearchBar.swift
@@ -46,6 +46,7 @@ struct SearchBar: View {
         }
     }
 }
+
 //struct SearchBar_Previews: PreviewProvider {
 //    static var previews: some View {
 //        SearchBar(searchText: .constant(""))
